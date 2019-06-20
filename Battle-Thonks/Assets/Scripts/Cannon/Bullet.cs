9031cf970c789ae2fe91ee3f1f8a100e5bd7855b
thomas_eddyson <2071098@talnet.nl>
2019-06-20 09:58:35 +0200

    Thonks-Shells

---
+++ Battle-Thonks/Assets/Scripts/Cannon/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{    
+    public int secondsToDestroy = 5;
+
+    public float speed = 70;
+
+
+    void Update()
+    {
+        Destroy(this, secondsToDestroy);
+        return;
+    }
+}
---
+++ Battle-Thonks/Assets/Scripts/Cannon/TurretTank.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretTank : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    public Transform firePoint;
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);            
+        }
+    }
+}
